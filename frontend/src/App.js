44c9bfd5d4028a24db19b3d8dd5cea4043eba9a4
Ankit Kumar <118598590+iamankit7667@users.noreply.github.com>
2024-02-13 19:32:18 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import NavbarItems from "./components/NavbarItems";
+import Navbar from "./components/Header/Navbar";
+import NavbarItems from "./components/Header/NavbarItems";
+import HeroSection from "./components/Hero-Section/HeroSection";
+import CompanySection from "./components/Company-section/Company";
+import AboutUs from "./components/About-us/AboutUs";
+import Courses from "./components/Courses-section/Courses";
+import ChooseUs from "./components/Choose-us/ChooseUs";
+import Features from "./components/Feature-section/Features";
+import FreeCourse from "./components/Free-course-section/FreeCourse";
+import Testimonials from "./components/Testimonial/Testimonials";
+import Newsletter from "./components/Newsletter/Newsletter";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   const current_theme = localStorage.getItem("current_theme");
@@ -8,11 +18,22 @@ const App = () => {
   useEffect(() => {
     localStorage.setItem("current_theme", theme);
   }, [theme]);
-
   return (
-    <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <NavbarItems />
+    <div>
+      <div className={`container ${theme}`}>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <NavbarItems />
+        <HeroSection />
+        <CompanySection />
+        <AboutUs />
+        <Courses />
+        <ChooseUs />
+        <Features />
+        <FreeCourse />
+        <Testimonials />
+      </div>
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/Header/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Navbar.css";
+import logo_light from "../../assests/logo-black.jpg";
+import logo_dark from "../../assests/logo-white.png";
+import search_icon_light from "../../assests/search-w.png";
+import search_icon_dark from "../../assests/search-b.png";
+import toggle_light from "../../assests/night.png";
+import toggle_dark from "../../assests/day.png";
+
+const Navbar = ({ theme, setTheme }) => {
+  const toggle_mode = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <div className="navbar">
+      <img
+        src={theme === "light" ? logo_light : logo_dark}
+        alt=""
+        className="logo"
+      />
+      <ul>
+        <li>Home</li>
+        <li>Tutorials</li>
+        <li>Excercises</li>
+        <li>Blog</li>
+        <li>About Us</li>
+      </ul>
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+        <img
+          src={theme === "light" ? search_icon_light : search_icon_dark}
+          alt=""
+          className="search-icon"
+        />
+      </div>
+
+      <img
+        onClick={() => {
+          toggle_mode();
+        }}
+        src={theme === "light" ? toggle_light : toggle_dark}
+        alt=""
+        className="toggle-icon"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
