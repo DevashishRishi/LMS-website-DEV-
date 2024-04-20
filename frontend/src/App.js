28287d33e7bba20c38cc0d55d5a1c83a1220738c
Ankit Kumar <118598590+iamankit7667@users.noreply.github.com>
2024-04-20 17:19:57 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Navbar from "./components/Header/Navbar";
 import NavbarItems from "./components/Header/NavbarItems";
-// import Footer from "./components/Footer/Footer";
 import Tutorials from "./pages/Tutorials";
 import AboutUs from "./pages/AboutUs";
 import Courses from "./pages/Courses";
@@ -17,20 +16,35 @@ import C from "./courses/C/C";
 import Java from "./courses/Java/Java";
 import LoginForm from "./pages/LoginForm";
 import SignupSection from "./pages/SignupSection";
+import Placement from "./pages/Placement";
+import ActivationPage from "./components/ActvationPage";
 
 const App = () => {
   const current_theme = localStorage.getItem("current_theme");
   const [theme, setTheme] = useState(current_theme ? current_theme : "light");
+  
   useEffect(() => {
     localStorage.setItem("current_theme", theme);
   }, [theme]);
+  
   return (
-    <BrowserRouter>
+    <Router>
       <div className={`mode ${theme}`}>
         <Navbar theme={theme} setTheme={setTheme} />
         <NavbarItems />
       </div>
+
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tutorials" element={<Tutorials />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/placement" element={<Placement />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/enroll" element={<Enroll />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupSection />} />
+        <Route path="/activation" element={<ActivationPage />} /> {/* Use element prop for rendering component */}
         <Route path="/deep_learning" element={<DeepLearning />} />
         <Route path="/machine_learning" element={<MachineLearning />} />
         <Route path="/python" element={<Python />} />
@@ -38,21 +52,7 @@ const App = () => {
         <Route path="/C++" element={<CPlusPlus />} />
         <Route path="/java" element={<Java />} />
       </Routes>
-      {/* <div className={`container ${theme}`}> */}
-      <div className={`mode ${theme}`}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tutorials" element={<Tutorials />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/enroll" element={<Enroll />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupSection />} />
-          <Route path="/enroll" element={<Enroll />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    </Router>
   );
 };
 
